a8d15c218c956a6669b6ebe8eafdc7d40af4e664
Naim <naimsantos45@gmail.com>
2020-09-19 10:06:47 -0300

    Remove prints

--- RK4_multiple2.cpp
+++ RK4_multiple2.cpp
@@ -8,7 +8,6 @@
 double f1(double x, double y, double z);
 double f2(double x, double y, double z);
 void solveRK4(const double h, double xi, const double xf, std::array<double, 2>& Ys);
-void print_txt(const double h, double xi, const double xf, std::array<double, 2>& Ys);
 
 int main(int argc, char* arg[]){
 	double xi {0.0};
@@ -17,16 +16,15 @@ int main(int argc, char* arg[]){
 	std::array<double, 2> Ys = {4.0, 6.0};
 	
 	solveRK4(h, xi, xf, Ys);
-	std::cout << "y = " << Ys[0] << std::endl;
-	std::cout << "z = " << Ys[1] << std::endl;
+	//std::cout << "y = " << Ys[0] << std::endl;
+	//std::cout << "z = " << Ys[1] << std::endl;
 }
 void solveRK4(const double h, double xi, const double xf, std::array<double, 2>& Ys){
 
 	const auto n = std::round((xf - xi) / h);//numero de intervalos a estimar
-	double k[2][4+1] = {0.0}; //last collumn used for the sum of the slopes
+	double k[2][4+1] = {0.0}; //last collumn used for the sum of the constants
 	//std::array<std::array<double, 4+1>, 2> k;
 
-	std::cout << "n = " << n << ", x0 = " << xi << ", xf = " << xf << ", y0 = " << Ys[0] << ", z0 = " << Ys[1]  << std::endl;
 	std::fstream printer {"output_rk4.csv", std::ios::app};
 	printer << "Solution via 4th Order Runge-Kutta Method.\n"; 
 	printer << "x_initial = " << xi << ", total steps = " << n << ", x_final = " << xf << '\n';
@@ -55,11 +53,6 @@ void solveRK4(const double h, double xi, const double xf, std::array<double, 2>&
 		printer << i << ',' << xi-h << ',' << Ys[0] << ','  << Ys[1] << '\n';
 		//std::cout << "x " << xi << ", y= " << Ys[0] << ", z= " << Ys[1] << std::endl;
 	}
-}
-void print_txt(std::array<double, 2>& Ys){
-	
-	
-	
 }
 double f1(double x, double y, double z){
 	return -0.5*y;
