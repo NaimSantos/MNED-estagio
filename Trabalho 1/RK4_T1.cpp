6cc08cb9520655fc695b6ea3bdd28803eefd3858
NaimSantos <naimsantos45@gmail.com>
2020-10-17 14:25:17 -0300

    Updates

--- Trabalho 1/RK4_T1.cpp
+++ Trabalho 1/RK4_T1.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <array>
 #include <fstream>
-#include <iomanip>
+//#include <iomanip>
 
 //Dados:
 const double Pr {10.0};
@@ -17,34 +17,30 @@ double f1(double tao, double x, double y, double z);
 double f2(double tao, double x, double y, double z);
 double f3(double tao, double x, double y, double z);
 void solveRK4(const double h, double t_i, const double t_f, std::array<double, 3>& S);
-
-int main(int argc, char* arg[]){
-	
-	std::array<double, 3> S = {X_0, Y_0, Z_0};	//inicializada com os valores dados
-	solveRK4(Tao_delt, Tao_0, Tao_f, S);
-
-}
 /*Parâmetros:
-	h = passo do método re Runge-Kutta
+	h = passo do método de Runge-Kutta
 	t_i = tempo inicial de avaliação;
 	t_f = tempo final;
 	S = um array com 3 elementos, para armazenar as 3 variáveis avaliadas.
 */
+
+int main(){
+
+	std::array<double, 3> S = {X_0, Y_0, Z_0};
+	solveRK4(Tao_delt, Tao_0, Tao_f, S);
+}
 void solveRK4(const double h, double t_i, const double t_f, std::array<double, 3>& S){
 
-	const auto n = std::round((t_f - t_i) / h);	//número de intervalos a estimar
-	std::array<std::array<double, 5>, 3> k;	//4 constantes do RK4 + 1 coluna para a soma dos ks
-	/*Alternativamente:
-		double k[N][5] = {0.0, 0.0, 0.0, 0.0, 0.0}; 
-	*/
-	
+	const auto n = std::round((t_f - t_i) / h); //número de intervalos a estimar
+	std::array<std::array<double, 5>, 3> k; //4 constantes do RK4 + 1 coluna para a soma dos ks
+
 	std::fstream printer {"output_rk4.dat", std::ios::app};
 	printer << "Solution via 4th Order Runge-Kutta Method.\n"; 
 	printer << "t_inicial = " << t_i << ", total steps = " << n << ", t_final = " << t_f << '\n';
 	printer << "Stept X Y Z\n";
-	
+
 	printer << "0 " << t_i << ' ' << S[0] << ' ' << S[1] << ' ' << S[2] << '\n';
-	
+
 	for (size_t i {1}; i <= n ; i++){
 
 		k[0][0] = f1(t_i, S[0], S[1], S[2]);
@@ -70,9 +66,9 @@ void solveRK4(const double h, double t_i, const double t_f, std::array<double, 3
 		S[0] = S[0] + k[0][4];
 		S[1] = S[1] + k[1][4];
 		S[2] = S[2] + k[2][4];
-		
+
 		t_i = t_i + h;
-		printer << i << ' ' << t_i << ' ' << S[0] << ' '  << S[1] << ' ' << S[2] << '\n';
+		printer << i << ' ' << t_i << ' ' << S[0] << ' ' << S[1] << ' ' << S[2] << '\n';
 	}
 }
 double f1(double tao, double x, double y, double z){
