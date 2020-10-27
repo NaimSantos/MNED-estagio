9bd00a93b27257a5fd379b89e7a569d376ed0d4a
NaimSantos <naimsantos45@gmail.com>
2020-10-27 13:45:33 -0300

    Improve FTCS method
    Separação de funções, renomeação de variáveis e uso de funções de C++

--- FTCS.cpp
+++ FTCS.cpp
@@ -2,36 +2,35 @@
 #include <cmath>
 #include <iomanip>
 #include <fstream>
-
-#include <cstdlib>
 #include <ctime>
 #include <cstring>
+#include <cstdlib>
 
 
 int i4_modp(int i, int j);
 int i4_wrap(int ival, int ilo, int ihi);
 double *initial_condition(int nx, double x[]);
 double *r8vec_linspace_new(int n, double a, double b);
-void time_capture();
+void time_capture(int key);
+void writegnuplot(const std::string& filename);
 
 int main (){
 	double a, b, c;
-	std::string command_filename = "comandos_adveccao.txt";
-	std::ofstream command_unit;
+	
 	std::string data_filename = "dados_adveccao.txt";
-	std::ofstream data_unit;
+	std::ofstream data_output;
 	double dt, dx;
 	int jm1, jp1, nx, nt, nt_step, t;
 	double *u;
 	double *unew;
 	double *x;
 
-	time_capture();
+	time_capture(0);
 
 	//Definição de variáveis:
 	nx = 101;
 	dx = static_cast<double>(1.0 / (nx-1));
-	a = 0.0, 
+	a = 0.0;
 	b = 1.0;
 	x = r8vec_linspace_new(nx, a, b);
 	nt = 1000;
@@ -42,22 +41,16 @@ int main (){
 	u = initial_condition(nx, x);
 
 	//Abre o arquivo e escreve as soluções enquanto elas são computadas:.
-	data_unit.open(data_filename.c_str ());
+	data_output.open(data_filename);
 
 	t = 0.0;
-	data_unit << "  " << x[0] << "  " << t << "  " << u[0] << "\n";
+	data_output << "  " << x[0] << "  " << t << "  " << u[0] << "\n";
 	for (int j = 0; j < nx; j++ ){
-		data_unit << "  " << x[j] << "  " << t << "  " << u[j] << "\n";
+		data_output << "  " << x[j] << "  " << t << "  " << u[j] << "\n";
 	}
-	data_unit << "\n";
+	data_output << "\n";
 
 	nt_step = 100;
-
-	std::cout << "\n";
-	std::cout << "  Numero de nos NX = " << nx << "\n";
-	std::cout << "  Numero de intervalos de tempo NT = " << nt << "\n";
-	std::cout << "  Velocidade constant c = " << c << "\n";
-
 	unew = new double[nx];
 
 	for (int i = 0; i < nt; i++ ){
@@ -72,45 +65,29 @@ int main (){
 		if (i == nt_step-1){
 			t =  i*dt;
 			for (int j = 0; j < nx; j++){
-				data_unit << "  " << x[j] << "  " << t << "  " << u[j] << "\n";
+				data_output << "  " << x[j] << "  " << t << "  " << u[j] << "\n";
 			}
-			data_unit << "\n";
+			data_output << "\n";
 			nt_step = nt_step + 100;
 		}
 	}
 
-	//Fechar o arquivo quando acabar de computar
-	data_unit.close();
+	//Fechar o arquivo quando acabar de computar:
+	data_output.close();
+	std::cout << "\n  Resultados exportados para \"" << data_filename << "\"\n";
 
-	std::cout << "\n  Plotar valores em um arquivo \"" << data_filename << "\"\n";
-
-	//Escreve a arquivo do gnuplot:
-
-	command_unit.open(command_filename.c_str());
-
-	command_unit << "set term png\n";
-	command_unit << "set output 'advection.png'\n";
-	command_unit << "set grid\n";
-	command_unit << "set style data lines\n";
-	command_unit << "unset key\n";
-	command_unit << "set xlabel '<---X--->'\n";
-	command_unit << "set ylabel '<---Time--->'\n";
-	command_unit << "splot '" << data_filename << "' using 1:2:3 with lines\n";
-	command_unit << "quit\n";
-
-	command_unit.close ();
-
-	std::cout << "  Comandos do Gnuplot foram escritos em um arquivo \"" << command_filename << "\"\n";
+	//Gerar um arquivo do gnuplot:
+	std::string command_filename = "comandos_adveccao.txt";
+	writegnuplot(command_filename);
+	std::cout << "  Comandos do Gnuplot exportados para \"" << command_filename << "\"\n\n";
 
 	//Desalocar as matrizes:
-
 	delete [] u;
 	delete [] unew;
 	delete [] x;
 
-	std::cout << "Fim de execucao normal.\n\n";
-	time_capture();
-
+	//Encerrar:
+	time_capture(1);
 	return 0;
 }
 
@@ -157,8 +134,8 @@ double *initial_condition(int nx, double x[]){
 	double *u = new double[nx];
 
 	for (int i = 0; i < nx; i++ ){
-		if ( 0.4 <= x[i] && x[i] <= 0.6 ){
-			u[i] = std::pow( 10.0 * x[i] - 4.0, 2 ) * std::pow ( 6.0 - 10.0 * x[i], 2 );
+		if ( 0.4<=x[i] && x[i]<=0.6 ){
+			u[i] = std::pow( 10.0*x[i] - 4.0, 2) * std::pow(6.0 - 10.0*x[i], 2);
 		}
 		else {
 			u[i] = 0.0;
@@ -182,16 +159,27 @@ double *r8vec_linspace_new(int n, double a_first, double a_last){
 	return a;
 }
 
-void time_capture(){
-	const int time_size {40};
-	static char time_buffer[time_size];
-	const struct std::tm *tm_ptr;
-	std::time_t now;
-
-	now = std::time(NULL);
-	tm_ptr = std::localtime (&now);
-
-	std::strftime (time_buffer, time_size, "%d %B %Y %I:%M:%S %p", tm_ptr);
-
-	std::cout << time_buffer << "\n";
+void time_capture(int key){
+	std::time_t now = std::time(nullptr);
+	
+	(key==0) ? (std::cout << "Inicio da execucao: ") : (std::cout << "Fim da execucao: ");
+	
+	std::cout << std::asctime(std::localtime(&now)) ;
+}
+void writegnuplot(const std::string& filename){
+	std::ofstream fwritter;
+	
+	fwritter.open(filename);
+	
+	fwritter << "set term png\n";
+	fwritter << "set output 'advection.png'\n";
+	fwritter << "set grid\n";
+	fwritter << "set style data lines\n";
+	fwritter << "unset key\n";
+	fwritter << "set xlabel '<---X--->'\n";
+	fwritter << "set ylabel '<---Time--->'\n";
+	fwritter << "splot '" << filename << "' using 1:2:3 with lines\n";
+	fwritter << "quit\n";
+	
+	fwritter.close();
 }
