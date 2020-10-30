a852789a9042b8bb8a11f63c8c988724cdff0a08
NaimSantos <naimsantos45@gmail.com>
2020-10-30 11:13:46 -0300

    Reestruturação

--- min_quad.h
+++ min_quad.h
@@ -1,3 +1,6 @@
+#ifndef GENERIC_ESTATIST_H
+#define GENERIC_ESTATIST_H
+
 #include <iostream>
 #include <cmath>
 #include <array>
@@ -22,16 +25,6 @@ double covar(T1* A1, T2* A2, const unsigned int n);
 
 template<typename T1, typename T2>
 LinReg RegressaoLinear(T1* A1, T2* A2, const unsigned int n);
-
-int main(int argc, char* arg[])
-{
-	int X[7] {1, 2, 3, 4, 5, 6, 7};
-	double Y[7] {2, 4.1, 5.8, 8.3, 10, 12.3, 14.3};
-	auto resultado = RegressaoLinear<int, double>(X, Y, std::size(X));
-	
-	std::cout << std::setprecision(10) << "a = " << resultado.a << "\b = " << resultado.b
-		<< "\r = " << resultado.r << "\eps = " << resultado.eps << std::endl;
-}
 
 template <typename T>
 T average(T* A, const unsigned int n){
@@ -74,27 +67,27 @@ LinReg RegressaoLinear(T1* A1, T2* A2, const unsigned int n){
 	auto desvx = desvio<T1>(A1, n, xmed);
 	auto desvy = desvio<T2>(A2, n, ymed);
 
-	//a
+	//Inclinação:
 	auto a = (covarxy / (desvx*desvx));
 	lin_reg.a = a;
 
-	//b
+	//Interseção:
 	auto b = ymed - (a * xmed);
 	lin_reg.b = b;
 
-	//r
+	//coeficiente de correlação de Pearson
 	auto r = covarxy /( desvx * desvy);
 	lin_reg.r = r;
 
-	//epsilon_y
-	{
-		double sum = 0.0;
-		for (int i = 0; i < n; i++){
-			sum = std::pow((A2[i] - ((a*A1[i]) + b)), 2) / (n-2);
-		}
-		auto eps = std::sqrt(sum);
-		lin_reg.eps = eps;
+	//erro padrão em y:
+	double sum = 0.0;
+	for (int i = 0; i < n; i++){
+		sum = std::pow((A2[i] - ((a*A1[i]) + b)), 2) / (n-2);
 	}
+	auto eps = std::sqrt(sum);
+	lin_reg.eps = eps;
+
 
 	return lin_reg;
 }
+#endif //GENERIC_ESTATIST_H
