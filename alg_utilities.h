7cd13d724c7616f5ef60187e1ed2abc567244059
Naim Santos <naimsantos45@gmail.com>
2020-11-03 11:53:20 -0300

    Formulação inicial para o método de thomas

--- alg_utilities.h
+++ alg_utilities.h
@@ -22,7 +22,7 @@ bool isdiagonaldom(double** M, const int nrows, const int ncol){
 	return true;
 }
 
-//print a bidimensional array of elements of type T
+//prints a bidimensional array of elements of type T
 template <typename T>
 void printarray2D(T** M, const int nrow, const int ncol){
 	for(int i=0; i<nrow; i++){
@@ -32,5 +32,31 @@ void printarray2D(T** M, const int nrow, const int ncol){
 		std::cout << '\n';
 	}
 }
+
+void thomas_solver(const std::vector<double>& a, const std::vector<double>& b,
+	const std::vector<double>& c,  const std::vector<double>& d, std::vector<double>& f)
+{
+	size_t N = d.size();
+
+
+	std::vector<double> c_start(N, 0.0);
+	std::vector<double> d_start(N, 0.0);
+ 
+	c_start[0] = c[0] / b[0];
+	d_start[0] = d[0] / b[0];
+
+ 
+	for (int i=1; i<N; i++) {
+		double m = 1.0 / (b[i] - a[i] * c_start[i-1]);
+		c_start[i] = c[i] * m;
+		d_start[i] = (d[i] - a[i] * d_start[i-1]) * m;
+	}
+
+                                                                                                                                              
+	for (int i=N-1; i-- > 0; ) {
+		f[i] = d_start[i] - c_start[i] * d[i+1];
+	}
+}
+
 
 #endif //HEADER_H
