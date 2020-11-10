37664faadcbc07ef0b5c3eeec0a658704b2dbb22
Naim Santos <naimsantos45@gmail.com>
2020-11-10 10:20:02 -0300

    Correção para o algoritmo de thomas

--- thomas.cpp
+++ thomas.cpp
@@ -9,21 +9,21 @@
 */
 using std::vector;
 
-void tdma_solver(const vector<double>& a, const vector<double>& b, vector<double>& c, vector<double>& d, vector<double>& f){
-	auto n = d.size()-1;
+void tdma_solver(const vector<double>& a, const vector<double>& b, vector<double>& c, vector<double>& d){
+	auto n = static_cast<int>(d.size()-1);
 
 	c[0] /= b[0];
 	d[0] /= b[0];
 
 	for (int i = 1; i < n; i++){
-		c[i] = c[i] / (b[i] - a[i]*c[i-1]);
+		c[i] = (c[i] ) / (b[i] - a[i]*c[i-1]);
 		d[i] = (d[i] - a[i]*d[i-1]) / (b[i] - a[i]*c[i-1]);
 	}
 
 	d[n] = (d[n] - a[n]*d[n-1]) / (b[n] - a[n]*c[n-1]);
 
-	for (int i = n; i > 0; i--){
-		d[i] = d[i] - c[i]*d[i+1];
+	for (int i = n; i-- > 0;){
+		d[i] = d[i] - (c[i]*d[i+1]);
 	}
 }
 int main(int argc, char* arg[]){
@@ -32,9 +32,8 @@ int main(int argc, char* arg[]){
 	vector<double> c1 = {1, 2, 1, 0};    //diagonal superior
 	
 	vector<double> d1 = {7, 19, 31, 52};
-	vector<double> f1 = {7, 19, 31, 52};
-	
-	tdma_solver(a1, b1, c1, d1, f1);
+
+	tdma_solver(a1, b1, c1, d1);
 
 	std::cout << "T = ";
 	for (auto &e: d1){
