2e7743a8e4df9e73df7e58972f1e6e9f25e37191
NaimSantos <naimsantos45@gmail.com>
2020-11-10 09:05:31 -0300

    Formulação do algoritmo de thomas

--- Trabalho 2/TDMA.cpp
+++ Trabalho 2/TDMA.cpp
@@ -6,11 +6,12 @@
 #include <algorithm>	//std::fill
 #include "alg_utilities.h"	//NPI
 
+using std::vector;
+
 double analytic_solution(double x, double t);
-void tdma_solver(const std::vector<double>& a, const std::vector<double>& b, std::vector<double>& c, const std::vector<double>& d,
-	std::vector<double>& f, std::vector<double>& c_temp, std::vector<double>& d_temp);
+void tdma_solver(const vector<double>& a, const vector<double>& b, vector<double>& c, vector<double>& d, vector<double>& f, vector<double>& c_temp, vector<double>& d_temp);
 void print_analytic();
-void print_numerical(const std::vector<double>& f);
+void print_numerical(const vector<double>& f);
 
 //Dados:
 constexpr double alfa = 0.1;
@@ -25,13 +26,13 @@ constexpr double deltax = 0.2;
 constexpr double sigma = 0; //0 ou 1
 
 //Determinação dos coeficientes a serem utilizados:
-constexpr auto C = u*deltat/deltax;
-constexpr auto s = alfa*deltat/(deltax*deltax);
-constexpr auto coef_a = 1+ 2*beta*(0.5*C*sigma + s);
+constexpr auto C = (u*deltat)/deltax;
+constexpr auto s = (alfa*deltat)/(deltax*deltax);
+constexpr auto coef_a = 1 + 2*beta*(0.5*C*sigma + s);
 constexpr auto coef_b = beta*(0.5*C*(sigma-1) + s);
-constexpr auto coef_c = beta*(0.5*C*(sigma+1) + s);
-constexpr auto coef_d1 = (1-beta)*(0.5*C*(sigma+1) + s);
-constexpr auto coef_d2 = 1-2*(1-beta)*(0.5*C*sigma + s);
+constexpr auto coef_c = beta*(0.5*C*(1+sigma) + s);
+constexpr auto coef_d1 = (1-beta)*(0.5*C*(1+sigma) + s);
+constexpr auto coef_d2 = 1 - 2*(1-beta)*(0.5*C*sigma + s);
 constexpr auto coef_d3 = (1-beta)*(0.5*C*(sigma-1) + s);
 
 int main(int argc, char **argv) {
@@ -39,65 +40,68 @@ int main(int argc, char **argv) {
 	//Discretização em N intervalos:
 	const size_t N = 11;
 	//Inicialização dos vetores a serem utilizados:
-	std::vector<double> a(N, coef_a);	//diagonal principal
-	std::vector<double> b(N-1, coef_b);	//diagonal secundária superior
-	std::vector<double> c(N-1, coef_c);	//diagonal secundária inferior
+	vector<double> a(N, coef_a);       //vetor da diagonal principal
+	vector<double> b(N, coef_b);       //vetor da diagonal secundária superior
+	vector<double> c(N, coef_c);       //vetor da diagonal secundária inferior
+	vector<double> d(N, 0.0);          //vetor dos termos independentes
+	vector<double> f(N, 0.0);          //vetor onde a solução será escrita
+	vector<double> c_temp(N, 0.0);     //vetor auxiliar, para evitar que c seja sobreescrito
+	vector<double> d_temp(N, 0.0);     //vetor auxiliar, para evitar que d seja sobreescrito
 
-	std::vector<double> d(N, 0.0);
-	std::vector<double> f(N, 0.0);	//vetor solução
-	std::vector<double> c_temp(N, 0.0);	//temporarios, para evitar que os vetores sejam sobreescritos
-	std::vector<double> d_temp(N, 0.0);	//temporarios, para evitar que os vetores sejam sobreescritos
-
-	//Temperaturas iniciais fornecidas:
-	f[0] = 1.0;	d[0] = 1.0;
-
+	b[N-1] = 0;	c[0] = 0; //Estes 2 vetores na verdade tem 1 elemento a menos que a diagonal principal
+	d[0] = T1; d[N-1] = T2;	//Estes são os valores de temperatura conhecidos
+	
 	std::cout << std::setprecision(6) << "T = ";
-	for (auto &e: f){
+	for (auto &e: f)
 		std::cout << e << ' ';
-	}
 	std::cout << std::endl;
 
 	for (size_t i=1; i<N-1; i++) { //de d[1] ate d[10]
 		d[i] = coef_d1*f[i-1] + coef_d2*f[i] + coef_d3*f[i+1] ;
 	}
 
-	tdma_solver(c, a, b, d, f, c_temp, d_temp);
+	tdma_solver(a, b, c, d, f, c_temp, d_temp);
+	//tdma_solver(a1, b1, c1, d1, f1);
 
 	std::cout << "T = ";
-	for (auto &e: f){
+	for (auto &e: d)
 		std::cout << e << ' ';
-	}
 
-	print_analytic();
-	print_numerical(f);
+	//print_analytic();
+	//print_numerical(f);
 	return 0;
 }
 /*Parâmetros:
-	a = diagonal inferior,
-	b = diagonal principal
-	c = diagonal superior
-	d = termos independentes
-	f = vetor solução
-	c_temp e d_temp = vetores temporários para evitar que os vetores principais sejam sobreescrios.
+	a = diagonal inferior, b = diagonal principal, c = diagonal superior, d = termos independentes, f = vetor solução
 */
-void tdma_solver(const std::vector<double>& a, const std::vector<double>& b, std::vector<double>& c, const std::vector<double>& d,
-	std::vector<double>& f, std::vector<double>& c_temp, std::vector<double>& d_temp){
-	auto N = d.size();
+void tdma_solver(const vector<double>& a, const vector<double>& b, vector<double>& c, vector<double>& d, vector<double>& f, vector<double>& c_temp, vector<double>& d_temp){
+	auto N = d.size() - 1;
 
-	std::fill(c_temp.begin(), c_temp.end(), 0.0);
-	std::fill(d_temp.begin(), d_temp.end(), 0.0);
+	c[0] = c[0] / b[0];
+	d[0] = d[0] / b[0];
+
+	std::fill(c_temp.begin(), c_temp.end(), 0.0);	//Reseta os vetores temporarios, apenas por precaução
+	std::fill(d_temp.begin(), d_temp.end(), 0.0);	//Reseta os vetores temporarios, apenas por precaução
 
 	c_temp[0] = c[0] / b[0];
 	d_temp[0] = d[0] / b[0];
 
-	for (int i = 1; i < N; i++){
-		double m = 1.0 / (b[i] - a[i] * c_temp[i-1]);
-		c_temp[i] = c[i] * m;
-		d_temp[i] = (d[i] - a[i] * d_temp[i-1]) * m;
+	for (int i = 1; i < n; i++){
+		c[i] = c[i] / (b[i] - a[i]*c[i-1]);
+		d[i] = (d[i] - a[i]*d[i-1]) / (b[i] - a[i]*c[i-1]);
 	}
-	for (int i = N-1; i > 0; i--){
-		f[i] = d_temp[i] - c_temp[i] * d[i+1];
+
+	d[n] = (d[n] - a[n]*d[n-1]) / (b[n] - a[n]*c[n-1]);
+
+	for (int i = n; i > 0; i--){
+		d[i] = d[i] - c[i]*d[i+1];
 	}
+	//Escreve em f a solução:
+	/*
+	for (int i = n; i > 0; i--){
+		f[i] = d[i] - c[i]*d[i+1];
+	}
+	*/
 }
 
 void print_analytic(){
@@ -112,7 +116,7 @@ void print_analytic(){
 		i++;
 	}
 }
-void print_numerical(const std::vector<double>& f){
+void print_numerical(const vector<double>& f){
 	std::fstream printer {"output_thomas_crank_nicolson.dat", std::ios::app};
 	printer << "Solucao Via Crank-Nicolson.\n";
 	printer << "Iteration x T\n";
@@ -132,45 +136,3 @@ double analytic_solution(double x, double t){
 	}
 	return 0.5 - (2/NPI) * sum;
 }
-
-
-/*
-	//Exibir antes da execução:
-	std::cout << "A: ";
-	for (auto &e: a){
-		std::cout << e << ' ';
-	}
-	std::cout << "\nB: ";
-	for (auto &e: b){
-		std::cout << e << ' ';
-	}
-	std::cout << "\nC: ";
-	for (auto &e: c){
-		std::cout << e << ' ';
-	}
-	std::cout << "\nD: ";
-	for (auto &e: d){
-		std::cout << e << ' ';
-	}
-	std::cout << "\n\n";
-
-	//Exibir após a execução:
-	std::cout << "\nA: ";
-	for (auto &e: a){
-		std::cout << e << ' ';
-	}
-	std::cout << "\nB: ";
-	for (auto &e: b){
-		std::cout << e << ' ';
-	}
-	std::cout << "\nC: ";
-	for (auto &e: c){
-		std::cout << e << ' ';
-	}
-	std::cout << "\nD: ";
-	for (auto &e: d){
-		std::cout << e << ' ';
-	}
-	std::cout << "\n\n";
-
-*/
